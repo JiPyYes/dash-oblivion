d7da1f26f305609a3782a7632e1e7c018ea1e993
rodion <eofabcd@gmail.com>
2026-05-21 12:28:36 +0300

    Добавление проекта на гитхаб

---
+++ src/route.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import clients from "./components/clients.vue";
+import finances from "./components/finances.vue";
+import review from "./components/review.vue";
+import sales from "./components/sales.vue";
+
+const routes = [
+    {path:'/',component:review,name:'review'},
+    {path:'/sales', component:sales, name:'sales'},
+    {path:'/finances',component:finances,name:'finances'},
+    {path:'/clients',component:clients,name:'clients'},
+
+    {path:'/:pathName(.*)', component: Page404, name:'404'},
+
+]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) =>{
+    next()
+})
